54fdbb6a5b69232292bb91d622fbc688bc724525
angelchinenye42 <angelchinenye42@gmail.com>
2024-12-05 11:52:46 +0000

    include light-statusbar from expo-status-bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 import CategoryScreen from '../screens/categoryScreen';
+import {StatusBar} from "expo-status-bar";
 
 export default function Index() {
   return (
+    <>
+    <StatusBar style="light" />
     <CategoryScreen />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
   },
 });
--- screens/categoryScreen.js
+++ screens/categoryScreen.js
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 15,
+    backgroundColor: "#24180f"
   },
 });
 
